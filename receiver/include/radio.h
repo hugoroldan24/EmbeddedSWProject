3cfaf4988ca2da89221e7e5fec40c84df5e9fc01
Hugo <hugoroldan060@gmail.com>
2025-06-08 12:32:34 +0200

    Update radio.h

--- receiver/include/radio.h
+++ receiver/include/radio.h
@@ -7,6 +7,8 @@
 #ifndef RADIO_H
 #define RADIO_H
 
+
+#include <stdint.h>
 void RF_Receiver_Init(void);
 void Radio_Listen(void);
 void writeRegister(uint8_t writeCommand,uint8_t conf);
@@ -14,4 +16,7 @@ void sendCommand(uint8_t command);
 void writeAddress(uint8_t pipe,uint8_t *addr,uint8_t size);
 uint8_t readRegister(uint8_t reg);
 void get_Received_Data(uint8_t *byte1,uint8_t *byte2);
+
+extern volatile int8_t availableData;   /*This flag is activated when the RF module places data in the RX_FIFO register*/
+
 #endif
