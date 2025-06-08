b2aeb96a74e02a53f337995267f4916402493663
Hugo <hugoroldan060@gmail.com>
2025-06-08 12:32:10 +0200

    Update pwm.h

--- receiver/include/pwm.h
+++ receiver/include/pwm.h
@@ -7,9 +7,15 @@
 #ifndef PWM_H
 #define PWM_H
 
+#include <stdint.h>
 void PWM_Init(void);
 void PWM_Timer0_Init(void);
 void PWM_Start(void);
 void Convert_Value_PWM(uint8_t ejeX, uint8_t ejeyY, volatile uint16_t *converted_valueA, volatile uint16_t *converted_valueB);
+
+typedef struct{
+   uint16_t sA;
+   uint16_t sB;
+} Servo;
 
 #endif
